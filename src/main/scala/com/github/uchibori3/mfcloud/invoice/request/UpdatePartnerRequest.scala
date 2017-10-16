39c10509307725a5942ae20009eeb827efce55d4
Uchibori3 <uch180r13@gmail.com>
2017-10-16 11:27:36 +0900

    Trivial fix

--- src/main/scala/com/github/uchibori3/mfcloud/invoice/request/UpdatePartnerRequest.scala
+++ src/main/scala/com/github/uchibori3/mfcloud/invoice/request/UpdatePartnerRequest.scala
@@ -1,29 +1,28 @@
 package com.github.uchibori3.mfcloud.invoice.request
 
 case class UpdatePartnerRequest(
-  partner: UpdatePartner
+    partner: UpdatePartner
 )
 
 case class UpdatePartner(
-  code: Option[String],
-  name: String,
-  name_kana: Option[String],
-  name_suffix: Option[String],
-  memo: Option[String],
-  departments: Option[Seq[UpdateDepartment]]
+    code: Option[String],
+    name: String,
+    name_kana: Option[String],
+    name_suffix: Option[String],
+    memo: Option[String],
+    departments: Option[Seq[UpdateDepartment]]
 )
 
 case class UpdateDepartment(
-  id: String,
-  name: Option[String],
-  zip: Option[String],
-  tel: Option[String],
-  prefecture: String,
-  address1: Option[String],
-  address2: Option[String],
-  person_title: Option[String],
-  person_name: Option[String],
-  email: Option[String],
-  cc_emails: Option[String],
+    id: String,
+    name: Option[String],
+    zip: Option[String],
+    tel: Option[String],
+    prefecture: String,
+    address1: Option[String],
+    address2: Option[String],
+    person_title: Option[String],
+    person_name: Option[String],
+    email: Option[String],
+    cc_emails: Option[String]
 )
-
